3a3544b1e1e2db4372e16e1a5b52555d2efdd4c0
Thibault Helleputte <thibault.helleputte@dnalytics.com>
2018-03-16 18:01:15 +0100

    Regenerating outputs based on cleaned code

--- ImportVST.R
+++ ImportVST.R
@@ -1,5 +1,43 @@
 # Import Cubase EQ presets from a .pxml VST file
+
+#================================================================================
+#  "THE BEER-WARE LICENSE" (Revision 42):
+#  <Thib@LesEnroules.Rocks> wrote this file, helped by <ben@LesEnroules.Rocks>.  
+#  As long as you retain this notice you can do whatever you want with this stuff. 
+#  If we meet some day, and you think this stuff is worth it, you can buy us a beer 
+#  in return. Thibault Helleputte
+# ================================================================================
+
+# ================================================================================
+#  CITATION : Give credits to 'Les Enroules' by complying to the rules of BEERWARE 
+#  License of this code, and also by liking their music/video/material on one of 
+#  the following media :
+#  Web: http://LesEnroules.Rocks
+#  Facebook: https://www.facebook.com/LesEnroules/
+#  Youtube: https://www.youtube.com/channel/UCl6Q5lQFa6EZphm7TIEN-ew
+#  Soundcloud: https://soundcloud.com/thib-hell/albums
+#================================================================================
+
 library(XML)
+
+# input: path to a Cubase EQ Presets ".pxml" file in VST format.
+readCubasePresets<-function(file){
+  xmlFile=xmlTreeParse(file)  
+  topXml <- xmlRoot(xmlFile)
+  topXml <- topXml[[1]][[1]][[3]]
+  ListOfPresets=list()
+  Names=c()
+  for(i in 1:length(topXml)){
+    Presets=topXml[[i]]
+    Name=getCubaseEQName(Presets[[1]])
+    Values=getCubaseEQValues(Presets[[2]][[2]])
+    ListOfPresets[[Name]]=Values
+  }
+  return(ListOfPresets)
+}
+
+#================================================================================
+# Helper functions
 
 getCubaseEQName<-function(input){
   Attrs<-xmlAttrs(input)
@@ -14,19 +52,4 @@ getCubaseEQValues<-function(input){
   }
   row.names(EQValues)=c("Band1","Band2","Band3","Band4")
   return(EQValues)
-}
-
-readCubasePresets<-function(file){
-  xmlFile=xmlTreeParse(file)  
-  topXml <- xmlRoot(xmlFile)
-  topXml <- topXml[[1]][[1]][[3]]
-  ListOfPresets=list()
-  Names=c()
-  for(i in 1:length(topXml)){
-    Presets=topXml[[i]]
-    Name=getCubaseEQName(Presets[[1]])
-    Values=getCubaseEQValues(Presets[[2]][[2]])
-    ListOfPresets[[Name]]=Values
-  }
-  return(ListOfPresets)
 }
--- README.md
+++ README.md
@@ -1,27 +1,6 @@
 # VST2LV2
 Translation of VST plugins into LV2 standard
 
-
-### AUTHOR: 
-
-Code written by Thibault Helleputte, drummer of the band 'Les Enroules', Belgium, EU, with help from Ben Caby, guitarist of 'Les Enroules'.
-
-### Contact
-
-thib at LesEnroules dot Rocks
-
-### CITATION: 
-
-Give credits to 'Les Enroules' by liking their music/video/material on one of the following media :
-* Web: http://LesEnroules.Rocks
-* Facebook: https://www.facebook.com/LesEnroules/
-* Youtube: https://www.youtube.com/channel/UCl6Q5lQFa6EZphm7TIEN-ew
-* Soundcloud: https://soundcloud.com/thib-hell/albums
-
-### LICENSE
-
-GNU LGPL V3
-
 ### Use case
 
 The aim is to translate preset files from VST to LV2 format, in the context of Digital Audio Workstations (DAW).
